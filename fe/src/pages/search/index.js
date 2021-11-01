39eb5f3913e22ae9782340e2b5cd4f220009ffe2
thoriq1520 <thoriq.fadhillah.tif20@polban.ac.id>
2021-11-01 07:38:48 +0700

    Profile

---
+++ fe/src/pages/search/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const Search = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Search</Text>
+        </View>
+    )
+}
+
+export default Search
+
+const styles = StyleSheet.create({})
---
+++ fe/src/pages/toolbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Toolbar = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Toolbar</Text>
+        </View>
+    )
+}
+
+export default Toolbar
+
+const styles = StyleSheet.create({})
