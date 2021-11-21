4b0eac24870aecd549a10bc0e0229951a926386c
iqbalalghifary <iqbalalghifary97@gmail.com>
2021-11-21 18:35:59 +0700

    Unfollow
    Unfollow

--- be/service/service.go
+++ be/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"be/config"
 	"be/datastruct"
+	"fmt"
 	"log"
 )
 
@@ -21,4 +22,66 @@ func TambahTeman(teman datastruct.Teman) int64 {
 	}
 
 	return teman.Pengirim_id
+}
+
+func TampilkanTeman(id int64) ([]datastruct.Teman, error) {
+	db := config.CreateConnection()
+
+	defer db.Close()
+
+	var list_teman []datastruct.Teman
+
+	sqlStatement := `SELECT * FROM teman WHERE pengirim_id=$1 AND status='approved'`
+
+	rows, err := db.Query(sqlStatement, id)
+
+	if err != nil {
+		log.Fatalf("tidak bisa mengeksekusi query. %v", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var teman datastruct.Teman
+
+		err = rows.Scan(&teman.Pengirim_id, &teman.Penerima_id, &teman.Requested_at, &teman.Responded_at, &teman.Status)
+
+		if err != nil {
+			log.Fatalf("tidak bisa mengambil data. %v", err)
+		}
+
+		list_teman = append(list_teman, teman)
+
+	}
+	return list_teman, err
+}
+
+func HapusTeman(teman datastruct.Teman) int64 {
+
+	// mengkoneksikan ke db postgres
+	db := config.CreateConnection()
+
+	// kita tutup koneksinya di akhir proses
+	defer db.Close()
+
+	// buat sql query
+
+	sqlStatement := `DELETE FROM teman WHERE pengirim_id=$1 AND penerima_id=$2`
+	// eksekusi sql statement
+	res, err := db.Exec(sqlStatement, teman.Pengirim_id, teman.Penerima_id)
+
+	if err != nil {
+		log.Fatalf("tidak bisa mengeksekusi query. %v", err)
+	}
+
+	// cek berapa jumlah data/row yang di hapus
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("tidak bisa mencari data. %v", err)
+	}
+
+	fmt.Printf("Total data yang terhapus %v", rowsAffected)
+
+	return penerima_id
 }
--- be/transport/transport.go
+++ be/transport/transport.go
@@ -1,59 +1,162 @@
 package transport
 
 import (
-	"aph-go-service/datastruct"
-	"aph-go-service/logging"
-	"aph-go-service/service"
+	"be/datastruct"
+	"be/logging"
+	"be/service"
+
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func TmbhTeman(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	var teman datastruct.Teman
 	var member datastruct.Member
 
 	err := json.NewDecoder(r.Body).Decode(&teman)
-
 	if err != nil {
 		log.Fatalf("Tidak bisa mendecode dari request body.  %v", err)
 	}
 
-	teman.Requested_at = logging.GetDateTimeNowInString()
-	teman.Responded_at = logging.GetDateTimeNowInString()
-
 	getApiMember := fmt.Sprintf("https://617774f89c328300175f5973.mockapi.io/api/sadhelx/member/%d", teman.Penerima_id)
-	response, err := http.Get(getApiMember)
-
-	if err != nil {
-		log.Fatalf("Tidak bisa mendecode dari request body member.  %v", err)
+	response, _ := http.Get(getApiMember)
+	if response.StatusCode != 200 {
+		w.WriteHeader(response.StatusCode)
+		w.Write([]byte("Not found"))
+		return
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
-
-	json.Unmarshal(responseData, &member)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	json.Unmarshal(responseData, &member)
+
+	teman.Requested_at = logging.GetDateTimeNowInString()
+	teman.Responded_at = logging.GetDateTimeNowInString()
 	if member.Isprivate {
 		teman.Status = "approved"
 	} else {
 		teman.Status = "pending"
 	}
 
-	insertID := service.TambahTeman(teman)
+	service.TambahTeman(teman)
 
 	logging.Log(fmt.Sprintf("%d mengirimkan pertemanan ke %d", teman.Pengirim_id, teman.Penerima_id))
 
 	res := datastruct.Response1{
-		ID_pengirim: insertID,
+		ID_pengirim: teman.Pengirim_id,
+		ID_penerima: teman.Penerima_id,
+		Is_private:  member.Isprivate,
 		Message:     "Data teman telah ditambahkan",
 	}
 
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(res)
+}
+
+func TmplknTeman(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	var list_member []datastruct.Member
+
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Tidak bisa mengubah dari string ke int.  %v", err)
+	}
+
+	list_teman, err := service.TampilkanTeman(int64(id))
+
+	if err != nil {
+		log.Fatalf("Tidak bisa mengambil data. %v", err)
+	}
+
+	for _, element := range list_teman {
+
+		var member datastruct.Member
+
+		getApiMember := fmt.Sprintf("https://617774f89c328300175f5973.mockapi.io/api/sadhelx/member/%d", element.Penerima_id)
+		response, _ := http.Get(getApiMember)
+		if response.StatusCode != 200 {
+			w.WriteHeader(response.StatusCode)
+			w.Write([]byte("Not found"))
+			return
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.Unmarshal(responseData, &member)
+
+		list_member = append(list_member, member)
+	}
+
+	var response datastruct.Response3
+	response.Status = 1
+	response.Message = "Success"
+	response.Data = list_member
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func HapusTeman(w http.ResponseWriter, r *http.Request) {
+
+	// kita ambil request parameter idnya
+	//params := mux.Vars(r)
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// konversikan ke int yang sebelumnya adalah string
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Tidak bisa mengubah dari string ke int.  %v", err)
+	}
+
+	//belum diubah
+	getApiMember := fmt.Sprintf("https://617774f89c328300175f5973.mockapi.io/api/sadhelx/member/%d", teman.Penerima_id)
+	response, _ := http.Get(getApiMember)
+	if response.StatusCode != 200 {
+		w.WriteHeader(response.StatusCode)
+		w.Write([]byte("Not found"))
+		return
+	}
+
+	// panggil fungsi hapusbuku , dan convert int ke int64
+	deletedRows := models.HapusTeman(int64(id))
+
+	// ini adalah format message berupa string
+	msg := fmt.Sprintf("Friend has been deleted. Total : %v", deletedRows)
+
+	// ini adalah format reponse message
+	res := datastruct.Response2{
+		ID_pengirim: teman.Pengirim_id,
+		ID_penerima: teman.Penerima_id,
+		Message:     "teman telah dihapus",
+	}
+
+	// send the response
 	json.NewEncoder(w).Encode(res)
 }
