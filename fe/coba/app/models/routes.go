c86791c7bef696ce04dc8e01e3ac81774b4dbe7c
AnggaGemilang <anggagemilang12@gmail.com>
2021-11-29 10:03:21 +0700

    merge fe done

--- fe/coba/app/models/routes.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import "github.com/wandanuriza/gosadhelx/app/controllers"
-
-func (server *Server) initializeRoutes() {
-	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
-}
