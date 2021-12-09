9314084b3e816f17a7751f7ee6ef01d47912458f
Thoriq Fadhillah <42139609+thoriq1520@users.noreply.github.com>
2021-12-09 07:43:23 +0700

    Update index.js

--- fe/screens/FindFriends/index.js
+++ fe/screens/FindFriends/index.js
@@ -1,75 +1,189 @@
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Text,
-  Button,
-  Image,
-  TextInput
-} from 'react-native'
+
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, TextInput, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import { Card, SearchBar } from 'react-native-elements';
 
 
+
+const apiKey = 'a40093f0-53ec-11ea-850a-fbf5bb8990ef';
+
+ const processExhibit = results => {
+  const processed = 
+   
+    results.records.map(r => ({
+      ...r,
+
+      id: r.objectnumber
+    }))
+  
+  return processed;
+};
 
 export default class FindFriends extends Component {
-    render() {
-        return (
 
-      <View 
-      style={styles.viewStyle}>
+  constructor (props: {}) {
+    super(props);
+
+    this.state = {
+        page: 1,
+        isLoading: true,
+        dataSource: [],
+        refreshing: true,
+        
+    };
+  }
+
+  componentDidMount () {
+    this.makeRequest();
+  }
+
+  makeRequest =  () => {
+
+    const { page, search } = this.state;
+
+    
+    const url =
+            `https://api.harvardartmuseums.org/object?apikey=${apiKey}` +
+            `&title=${search}`+
+            `&fields=objectnumber,dated,century,division,primaryimageurl,title` +
+            `&sort=totalpageviews` +
+            `&page=${page}` +
+            `&size=44` +
+            `&hasimage=1` +
+            `&sortorder=desc`;
+    setTimeout(() => {
+      fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const results = processExhibit(responseJson);
+        this.setState({
+          isLoading: false,
+          dataSource: results,
+          refreshing: false
+        });
+       
+      });
+    }, 1500);
+
+  }
+
+  renderItem = ({item, index}) => {
+
+    if (!item) {
+      return null
+    }
+    
+    return (
+      <View >
+        <Card
+          title={item.title}
+          image={{uri: item.primaryimageurl}}>
+          <Text style={{marginBottom: 10}}>
+            Made in {item.dated}
+          </Text>
+       </Card>
+      </View>
+    );
+  }
+
+  keyExtractor = (item, index) => {
+    return index.toString();
+  }
+
+  updateSearch = search => {
+    this.setState({ search });  
+    this.makeRequest(search);
+  };
+
+  handleLoadMore = () => {
+
+    this.setState({ page: this.state.page + 1}, this.makeRequest)
+
+  }
+
+  handleRefresh = () => {
+
+    this.setState({
+      page: 1,
+      refreshing: true,
+      
+    }),
+    () =>
+      this.makeRequest();
+  }
+ 
+
+  render () {
+    const { search } = this.state;
+    return (
+     
+      <View style={styles.viewStyle}>
         <TextInput
           style={styles.textInputStyle}
-          // onChangeText={text => this.SearchFilterFunction(text)}
-          // value={this.state.text}
+          onChangeText={this.updateSearch}
+          value={search}
           underlineColorAndroid="transparent"
-          placeholder="Cari Teman"
+          placeholder="Find Friends"
         />
-
-
+        {/* <SearchBar
+            placeholder="Type something here...."
+            onChangeText={this.updateSearch}
+            value={search}
+            lightTheme
+            round
+            searchIcon={{ size: 24 }}
+        /> */}
+        <FlatList
+          data={this.state.dataSource}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndThreshold={100}
+        />
       </View>
-        )
-    }
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#8fccbbc'
-    },
-    border: {
-        borderBottomWidth: 1,
-        padding: 5,
-        backgroundColor: '#ff1694',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        borderColor: '#ff1694',
-        position: 'relative',
-        borderRadius: 10
-  },
-  search: {
-      position: 'absolute',
-      fontSize: 30,
-      fontWeight: '900',
-      color: '#ff1694'
-      
-  },
-   viewStyle: {
+  viewStyle: {
     justifyContent: 'center',
-    // flex: 1,
+
+    marginTop: 20,
+    padding: 16,
+  },
+  textStyle: {
+    padding: 5,
+    marginHorizontal: 5,
+    fontWeight: '900',
+    left: 60
+  },
+  textburik: {
+    fontWeight: '500',
+    padding: 10,
+     left: 60
+  },
+
+  gambar: {
+    top: 10,
+    padding: 15,
+    width: 50,
+    height:50,
+    borderRadius: 40,
+    position: 'absolute'
+
   },
   textInputStyle: {
     height: 40,
-    top: 10,
-    borderWidth: 2,
-    paddingLeft: 20,
+    top: -10,
+    borderWidth: 1,
+    paddingLeft: 10,
     borderColor: '#080808',
     backgroundColor: '#FFFFFF',
-    borderRadius: 15,
+    borderRadius: 20,
     left: 50,
-    width: 350
-    
+    width: 330
   },
-  
-})
+});
