10608c11ac117026e190ef3251d533ce3496f602
thoriq1520 <thoriq.fadhillah.tif20@polban.ac.id>
2021-12-15 15:20:02 +0700

    Update search.js

--- fe/screens/search.js
+++ fe/screens/search.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View, Button, Image } from 'react-native'
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
+
 export default class Search extends Component {
     render() {
         return (
-            <View>
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('FindFriends')}>
-                    <View
-                        style={styles.pencarian}>
-                        <Text style={styles.teksPencarian}>Search</Text>
-                        <Image source={require('../assets/icons/sear.png')} style={styles.icon} />
-                    </View>
-                </TouchableOpacity>
+            <View style={{ flex: 1 }}>
+
+                <View style={styles.borderP}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('FindFriends')}>
+                        <View
+                            style={styles.pencarian}>
+                            <Text style={styles.teksPencarian}>Search</Text>
+                            <Image source={require('../assets/icons/sear.png')} style={styles.icon} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
 
             </View>
         )
@@ -24,14 +29,10 @@ export default class Search extends Component {
 
 const styles = StyleSheet.create({
     pencarian: {
-        marginVertical: 100,
-        marginHorizontal: 20,
         width: 373,
         height: 50,
-        backgroundColor: '#fff',
-        borderRadius: 4,
-        borderColor: '#000',
         borderWidth: 1,
+        borderRadius: 8
     },
     teksPencarian: {
         position: 'absolute',
@@ -43,4 +44,10 @@ const styles = StyleSheet.create({
         left: 333,
         top: 15,
     },
+    borderP: {
+        width: 373,
+        height: 50,
+        top: 100,
+        left: 20,
+    }
 })
