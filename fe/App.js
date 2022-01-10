b4e4cf443cf17b1ca9d820eb414da0ddf87bd3fa
AnggaGemilang <anggagemilang12@gmail.com>
2022-01-10 14:19:37 +0700

    FE redesign profile flexbox

--- fe/App.js
+++ fe/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import Router from './router';
 import Profile from './screens/Profile';
 import HomeTab from './router'
 
+const navTheme = DefaultTheme;
+navTheme.colors.background = '#FFFFFF';
+
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      theme={navTheme}>
       <HomeTab />
     </NavigationContainer>
   );
